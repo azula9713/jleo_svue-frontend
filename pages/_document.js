2719c3ec1261fdd319be9c3a51f66fe1511525c1
Nilesh Viduranga <nilesh.viduranga@akqa.com>
2022-06-09 15:03:38 +0530

    Added custom configuration to laod css faster

--- pages/_document.js
+++ pages/_document.js
@@ -1,34 +1,62 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
 
-export default function Document() {
-  return (
-    <Html>
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="use-credentials"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap"
-          rel="stylesheet"
-        />
-        <meta
-          name="description"
-          content="We provide Planning Drawing and Submission, Interior & Architectural Design and Supply & Installation"
-        />
-        <meta content="Interior Design Studio" property="og:title" />
-        <meta
-          property="og:image"
-          content="https://static.wixstatic.com/media/b6fff4_f3bbc1cc480145648db2c761fc438350~mv2.png/v1/fill/w_1080,h_1080,al_c/b6fff4_f3bbc1cc480145648db2c761fc438350~mv2.png"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        })
+
+      const initialProps = await Document.getInitialProps(ctx)
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      }
+    } finally {
+      sheet.seal()
+    }
+  }
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="use-credentials"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap"
+            rel="stylesheet"
+          />
+          <meta
+            name="description"
+            content="We provide Planning Drawing and Submission, Interior & Architectural Design and Supply & Installation"
+          />
+          <meta content="Interior Design Studio" property="og:title" />
+          <meta
+            property="og:image"
+            content="https://static.wixstatic.com/media/b6fff4_f3bbc1cc480145648db2c761fc438350~mv2.png/v1/fill/w_1080,h_1080,al_c/b6fff4_f3bbc1cc480145648db2c761fc438350~mv2.png"
+          />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
 }
