bccbd02dcaa14a6d6901e0605d61d9d55e757928
Nilesh Viduranga <nilesh.viduranga@akqa.com>
2022-06-16 11:26:08 +0530

    Started architecture

--- pages/case-study/architecture.js
+++ pages/case-study/architecture.js
@@ -1,4 +1,6 @@
 import Head from 'next/head'
+import LetsTalk from '../../components/Common/LetsTalk'
+import Footer from '../../components/Layout/Footer'
 
 import Header from '../../components/Layout/Header'
 
@@ -9,7 +11,15 @@ const Architecture = () => {
         <title>Case Study - Architecture</title>
       </Head>
       <Header />
-      <div>Architecture</div>
+      <main className="main">
+        <div>Hero</div>
+        <div>
+          <div>1st potion gallery</div>
+          <div>2nd potion gallery</div>
+        </div>
+        <LetsTalk bgImage="/images/lets-talk-bg.jpeg" />
+      </main>
+      <Footer />
     </>
   )
 }
