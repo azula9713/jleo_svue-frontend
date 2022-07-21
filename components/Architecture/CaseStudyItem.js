742b95b95febe629940ce0981b362b4b990080ad
nilesh-swivel <nilesh@swivelgroup.com.au>
2022-07-21 13:37:25 +0530

    Css fixes

--- components/Architecture/CaseStudyItem.js
+++ components/Architecture/CaseStudyItem.js
@@ -73,6 +73,10 @@ const Container = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.md}) {
+    height: auto;
+  }
 `
 const SideImage = styled.div`
   position: relative;
